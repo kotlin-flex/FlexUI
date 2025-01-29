60d95c85caccb18ed5c7d10ceec6e746bfc4aabf
李佳伟 <2965502228@qq.com>
2025-01-30 05:15:51 +0800

    fix: 输入框改为使用 decorationBox 处理

--- flex-ui/src/commonMain/kotlin/cn/vividcode/multiplatform/flex/ui/foundation/input/FlexInput.kt
+++ flex-ui/src/commonMain/kotlin/cn/vividcode/multiplatform/flex/ui/foundation/input/FlexInput.kt
@@ -27,6 +27,8 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.Dp
@@ -49,199 +51,201 @@ fun FlexInput(
 	enabled: Boolean = true,
 	readOnly: Boolean = false,
 	maxLength: Int = Int.MAX_VALUE,
+	filterRegex: Regex? = null,
 	placeholder: @Composable (() -> Unit)? = null,
 	leadingIcon: FlexInputIcon? = null,
 	trailingIcon: FlexInputIcon? = null,
 	prefix: @Composable (() -> Unit)? = null,
 	suffix: @Composable (() -> Unit)? = null,
-	visualTransformation: VisualTransformation = VisualTransformation.None,
 	keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
 	keyboardActions: KeyboardActions = KeyboardActions.Default,
+	visualTransformation: VisualTransformation = VisualTransformation.None,
 ) {
 	val current = LocalFlexConfig.current
 	val config = current.input.getConfig(sizeType)
 	val targetColor = current.theme.colorScheme.current.getColor(colorType)
-	val minWidth by animateDpAsState(config.minWidth)
-	val height by animateDpAsState(config.height)
-	val borderWidth by animateDpAsState(config.borderWidth)
-	val fontSize by animateFloatAsState(config.fontSize.value)
-	val letterSpacing by animateFloatAsState(config.letterSpacing.value)
-	val horizontalPadding by animateDpAsState(config.horizontalPadding)
-	val corner by animateDpAsState(height * cornerType.percent)
-	val cornerShape by remember(corner) {
-		derivedStateOf {
-			RoundedCornerShape(corner)
-		}
-	}
-	val textFieldInteractionSource = remember { MutableInteractionSource() }
-	val leadingIconInteractionSource = remember { MutableInteractionSource() }
-	val trailingIconInteractionSource = remember { MutableInteractionSource() }
-	val textFieldIsFocused by textFieldInteractionSource.collectIsFocusedAsState()
-	val leadingIconIsFocused by leadingIconInteractionSource.collectIsFocusedAsState()
-	val trailingIconIsFocused by trailingIconInteractionSource.collectIsFocusedAsState()
 	val color by animateColorAsState(targetColor)
-	val isFocused = textFieldIsFocused || leadingIconIsFocused || trailingIconIsFocused
-	val borderColor by animateColorAsState(
-		targetValue = if (isFocused) targetColor else targetColor.copy(alpha = 0f)
-	)
-	val backgroundColor by animateColorAsState(
-		targetValue = if (isFocused) Color.Gray.copy(alpha = 0f) else Color.Gray.copy(alpha = 0.15f),
-	)
-	val textStyle by remember(fontSize, config.fontWeight, letterSpacing, targetColor) {
-		derivedStateOf {
-			TextStyle(
-				fontSize = fontSize.sp,
-				fontWeight = config.fontWeight,
-				letterSpacing = if (letterSpacing >= 0f) letterSpacing.sp else TextUnit.Unspecified,
-				color = targetColor
-			)
-		}
-	}
-	val iconSize by animateDpAsState(config.iconSize)
-	val interval = horizontalPadding / 2
-	Row(
-		modifier = Modifier
-			.widthIn(min = minWidth)
-			.height(height)
-			.border(
-				width = borderWidth,
-				color = borderColor,
-				shape = cornerShape
-			)
-			.background(
-				color = backgroundColor,
-				shape = cornerShape
-			)
-			.then(modifier)
-			.padding(horizontal = interval),
-		verticalAlignment = Alignment.CenterVertically
+	
+	CompositionLocalProvider(
+		LocalTextSelectionColors provides TextSelectionColors(
+			handleColor = color,
+			backgroundColor = color.copy(alpha = 0.15f)
+		)
 	) {
+		val minWidth by animateDpAsState(config.minWidth)
+		val height by animateDpAsState(config.height)
+		
+		val textFieldInteractionSource = remember { MutableInteractionSource() }
+		val leadingIconInteractionSource = remember { MutableInteractionSource() }
+		val trailingIconInteractionSource = remember { MutableInteractionSource() }
+		val textFieldIsFocused by textFieldInteractionSource.collectIsFocusedAsState()
+		val leadingIconIsFocused by leadingIconInteractionSource.collectIsFocusedAsState()
+		val trailingIconIsFocused by trailingIconInteractionSource.collectIsFocusedAsState()
+		val isFocused = textFieldIsFocused || leadingIconIsFocused || trailingIconIsFocused
+		
 		val focusRequester = remember { FocusRequester() }
-		if (leadingIcon != null) {
-			FlexInputIcon(
-				inputIcon = leadingIcon,
-				iconSize = iconSize,
-				iconTint = targetColor,
-				isFocused = isFocused,
-				focusRequester = focusRequester,
-				interactionSource = leadingIconInteractionSource
-			)
-		}
-		if (prefix != null) {
-			Spacer(modifier = Modifier.width(interval))
-			CompositionLocalProvider(
-				LocalContentColor provides color,
-				LocalTextStyle provides textStyle
-			) {
-				prefix()
+		val fontSize by animateFloatAsState(config.fontSize.value)
+		val letterSpacing by animateFloatAsState(config.letterSpacing.value)
+		val textStyle by remember(fontSize, config.fontWeight, letterSpacing, color) {
+			derivedStateOf {
+				TextStyle(
+					fontSize = fontSize.sp,
+					fontWeight = config.fontWeight,
+					letterSpacing = if (letterSpacing >= 0f) letterSpacing.sp else TextUnit.Unspecified,
+					color = color
+				)
 			}
 		}
-		Spacer(modifier = Modifier.width(interval))
-		CompositionLocalProvider(
-			LocalTextSelectionColors provides TextSelectionColors(
-				handleColor = Color.White,
-				backgroundColor = color.copy(alpha = 0.2f)
-			)
-		) {
-			BasicTextField(
-				value = value,
-				onValueChange = {
-					if (it.length <= maxLength) {
-						onValueChange(it)
+		
+		BasicTextField(
+			value = value,
+			onValueChange = {
+				if (it.length <= maxLength && filterRegex?.matches(it) != false) {
+					onValueChange(it)
+				}
+			},
+			modifier = Modifier
+				.focusRequester(focusRequester),
+			enabled = enabled,
+			readOnly = readOnly,
+			textStyle = textStyle,
+			keyboardOptions = keyboardOptions,
+			keyboardActions = keyboardActions,
+			singleLine = true,
+			visualTransformation = visualTransformation,
+			interactionSource = textFieldInteractionSource,
+			cursorBrush = SolidColor(color),
+			decorationBox = @Composable { innerTextField ->
+				val borderWidth by animateDpAsState(config.borderWidth)
+				val corner by animateDpAsState(height * cornerType.percent)
+				val cornerShape by remember(corner) {
+					derivedStateOf { RoundedCornerShape(corner) }
+				}
+				val borderColor by animateColorAsState(
+					targetValue = if (isFocused) targetColor else targetColor.copy(alpha = 0f),
+				)
+				val backgroundColor by animateColorAsState(
+					targetValue = if (isFocused) Color.Gray.copy(alpha = 0f) else Color.Gray.copy(alpha = 0.15f),
+				)
+				val interval by animateDpAsState(config.horizontalPadding / 2)
+				Row(
+					modifier = Modifier
+						.widthIn(min = minWidth)
+						.height(height)
+						.border(
+							width = borderWidth,
+							color = borderColor,
+							shape = cornerShape
+						)
+						.background(
+							color = backgroundColor,
+							shape = cornerShape
+						)
+						.then(modifier)
+						.padding(horizontal = interval + borderWidth),
+					verticalAlignment = Alignment.CenterVertically,
+				) {
+					val iconSize by animateDpAsState(config.iconSize)
+					if (leadingIcon != null) {
+						FlexInputIcon(
+							icon = leadingIcon.icon,
+							size = iconSize,
+							tint = leadingIcon.tint ?: targetColor,
+							onClick = leadingIcon.onClick,
+							isFocused = isFocused,
+							focusRequester = focusRequester,
+							interactionSource = leadingIconInteractionSource
+						)
 					}
-				},
-				modifier = Modifier
-					.focusRequester(focusRequester),
-				enabled = enabled,
-				readOnly = readOnly,
-				textStyle = textStyle,
-				keyboardActions = keyboardActions,
-				keyboardOptions = keyboardOptions,
-				singleLine = true,
-				visualTransformation = visualTransformation,
-				interactionSource = textFieldInteractionSource,
-				cursorBrush = SolidColor(targetColor),
-				decorationBox = @Composable { innerTextField ->
-					Box(
-						modifier = Modifier,
-						contentAlignment = Alignment.CenterStart
-					) {
-						if (placeholder != null) {
-							val isEmpty by remember(value) {
-								derivedStateOf { value.isEmpty() }
-							}
-							if (isEmpty) {
-								CompositionLocalProvider(
-									LocalTextStyle provides textStyle.copy(
-										color = color.copy(alpha = 0.6f)
-									)
-								) {
-									placeholder()
-								}
+					if (prefix != null) {
+						Spacer(modifier = Modifier.width(interval))
+						CompositionLocalProvider(
+							LocalContentColor provides color,
+							LocalTextStyle provides textStyle
+						) {
+							prefix()
+						}
+					}
+					Spacer(modifier = Modifier.width(interval))
+					
+					Box {
+						val isEmpty by remember(value) {
+							derivedStateOf { value.isEmpty() }
+						}
+						if (placeholder != null && isEmpty) {
+							CompositionLocalProvider(
+								LocalTextStyle provides textStyle.copy(
+									color = color.copy(alpha = 0.6f)
+								)
+							) {
+								placeholder()
 							}
 						}
 						innerTextField()
 					}
+					
+					Spacer(modifier = Modifier.width(interval))
+					if (suffix != null) {
+						CompositionLocalProvider(
+							LocalContentColor provides color,
+							LocalTextStyle provides textStyle
+						) {
+							suffix()
+						}
+						Spacer(modifier = Modifier.width(interval))
+					}
+					if (trailingIcon != null) {
+						FlexInputIcon(
+							icon = trailingIcon.icon,
+							size = iconSize,
+							tint = trailingIcon.tint ?: targetColor,
+							onClick = trailingIcon.onClick,
+							isFocused = isFocused,
+							focusRequester = focusRequester,
+							interactionSource = trailingIconInteractionSource
+						)
+					}
 				}
-			)
-		}
-		Spacer(modifier = Modifier.width(interval))
-		if (suffix != null) {
-			CompositionLocalProvider(
-				LocalContentColor provides color,
-				LocalTextStyle provides textStyle
-			) {
-				suffix()
 			}
-			Spacer(modifier = Modifier.width(interval))
-		}
-		if (trailingIcon != null) {
-			FlexInputIcon(
-				inputIcon = trailingIcon,
-				iconSize = iconSize,
-				iconTint = targetColor,
-				isFocused = isFocused,
-				focusRequester = focusRequester,
-				interactionSource = trailingIconInteractionSource
-			)
-		}
+		)
 	}
 }
 
 @Composable
 private fun FlexInputIcon(
-	inputIcon: FlexInputIcon,
-	iconSize: Dp,
-	iconTint: Color,
+	icon: ImageVector,
+	size: Dp,
+	tint: Color,
+	onClick: (() -> Unit)?,
 	isFocused: Boolean,
 	focusRequester: FocusRequester,
 	interactionSource: MutableInteractionSource,
 ) {
 	val isPressed by interactionSource.collectIsPressedAsState()
-	val targetIconColor = inputIcon.tint ?: iconTint
-	val iconColor by animateColorAsState(
+	val iconTint by animateColorAsState(
 		targetValue = when {
-			!isFocused -> targetIconColor.copy(alpha = 0.6f)
-			isPressed -> targetIconColor.copy(alpha = 0.8f)
-			else -> targetIconColor
+			!isFocused -> tint.copy(alpha = 0.6f)
+			isPressed -> tint.copy(alpha = 0.8f)
+			else -> tint
 		}
 	)
 	Icon(
-		imageVector = inputIcon.icon,
+		imageVector = icon,
 		contentDescription = null,
 		modifier = Modifier
-			.size(iconSize)
+			.size(size)
+			.pointerHoverIcon(PointerIcon.Default)
 			.then(
-				if (inputIcon.onClick == null) Modifier else Modifier.clickable(
+				if (onClick == null) Modifier else Modifier.clickable(
 					interactionSource = interactionSource,
 					indication = null,
 					onClick = {
-						inputIcon.onClick()
+						onClick()
 						focusRequester.requestFocus()
 					}
 				)
 			),
-		tint = iconColor
+		tint = iconTint
 	)
 }
 
