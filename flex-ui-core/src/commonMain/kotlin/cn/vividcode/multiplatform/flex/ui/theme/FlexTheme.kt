4c7d03829001a0c4a29830953e609de351f64bbe
李佳伟 <2965502228@qq.com>
2025-02-01 23:45:29 +0800

    fix: 修复错误

--- flex-ui-core/src/commonMain/kotlin/cn/vividcode/multiplatform/flex/ui/theme/FlexTheme.kt
+++ flex-ui-core/src/commonMain/kotlin/cn/vividcode/multiplatform/flex/ui/theme/FlexTheme.kt
@@ -1,40 +1,44 @@
 package cn.vividcode.multiplatform.flex.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.*
 import cn.vividcode.multiplatform.flex.ui.config.FlexConfig
 import cn.vividcode.multiplatform.flex.ui.config.LocalFlexConfig
 import cn.vividcode.multiplatform.flex.ui.config.flexConfig
+import androidx.compose.material3.darkColorScheme as defaultDarkColorScheme
+import androidx.compose.material3.lightColorScheme as defaultLightColorScheme
 
 @Composable
 fun FlexTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
-	lightColorScheme: ColorScheme = lightColorScheme(),
-	darkColorScheme: ColorScheme = darkColorScheme(),
+	lightColorScheme: () -> ColorScheme = { defaultLightColorScheme() },
+	darkColorScheme: () -> ColorScheme = { defaultDarkColorScheme() },
 	shapes: Shapes = MaterialTheme.shapes,
 	typography: Typography = MaterialTheme.typography,
 	flexConfig: FlexConfig = flexConfig(),
 	content: @Composable () -> Unit,
 ) {
-	val targetDarkTheme = FlexThemeState.darkTheme ?: darkTheme.also {
-		FlexThemeState.darkTheme = it
+	LaunchedEffect(darkTheme) {
+		FlexThemeState.darkTheme = darkTheme
 	}
 	CompositionLocalProvider(
-		LocalDarkTheme provides targetDarkTheme,
+		LocalDarkTheme provides FlexThemeState.darkTheme,
 		LocalFlexConfig provides flexConfig
 	) {
 		MaterialTheme(
-			colorScheme = if (targetDarkTheme) darkColorScheme else lightColorScheme,
+			colorScheme = if (LocalDarkTheme.current) darkColorScheme() else lightColorScheme(),
 			shapes = shapes,
 			typography = typography,
-		) {
-			content()
-		}
+			content = content
+		)
 	}
 }
 
 object FlexThemeState {
 	
-	var darkTheme by mutableStateOf<Boolean?>(null)
+	var darkTheme by mutableStateOf(false)
 }
